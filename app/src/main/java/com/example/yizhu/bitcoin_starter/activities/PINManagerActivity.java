19d96e78d66862dd32a658c46a1ea07d5d3ddf36
zack zhu <36757049@qq.com>
2018-08-18 00:07:52 -0700

    init

---
+++ app/src/main/java/com/example/yizhu/bitcoin_starter/activities/PINManagerActivity.java
@@ -0,0 +1,15 @@
+package com.example.yizhu.bitcoin_starter.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yizhu.bitcoin_starter.R;
+
+public class PINManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pin_manager);
+    }
+}
